6bc4e839807fecdee9f9d47fe2efb1a77df6dcef
Charl913 <charlkruger61@gmail.com>
2023-10-31 15:52:41 +0200

    Recipe Details & Work On ShoppingListComponent

--- src/app/content/recipe/recipe-lists/recipe-lists.component.ts
+++ src/app/content/recipe/recipe-lists/recipe-lists.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListsComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe('This is a recipe', 'This is more detail about the recipe', 'https://media.istockphoto.com/id/1479912027/photo/stir-fry-chicken-with-paprika-mushrooms-green-chives-and-sesame-seeds-in-ceramic-bowl-asian.webp?s=2048x2048&w=is&k=20&c=cvK2WbLmumldC1gGJEPFAZ7fakIAJayVv8U7Xxakyuo='),
     new Recipe('This is a recipe', 'This is more detail about the recipe', 'https://media.istockphoto.com/id/1479912027/photo/stir-fry-chicken-with-paprika-mushrooms-green-chives-and-sesame-seeds-in-ceramic-bowl-asian.webp?s=2048x2048&w=is&k=20&c=cvK2WbLmumldC1gGJEPFAZ7fakIAJayVv8U7Xxakyuo=')
   ];
   constructor() { }

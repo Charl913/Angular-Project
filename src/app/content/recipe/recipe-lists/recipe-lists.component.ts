ac52cd91376bd5bd2c9a3c0503ec858ffbcf05c2
Charl913 <charlkruger61@gmail.com>
2023-12-12 14:59:49 +0200

    Pass Data

--- src/app/content/recipe/recipe-lists/recipe-lists.component.ts
+++ src/app/content/recipe/recipe-lists/recipe-lists.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListsComponent implements OnInit {
   @Output()
+  recipeWasSelected = new EventEmitter<Recipe>();  
   recipes: Recipe[] = [
     new Recipe('This is a recipe', 'This is more detail about the recipe', 'https://media.istockphoto.com/id/1479912027/photo/stir-fry-chicken-with-paprika-mushrooms-green-chives-and-sesame-seeds-in-ceramic-bowl-asian.webp?s=2048x2048&w=is&k=20&c=cvK2WbLmumldC1gGJEPFAZ7fakIAJayVv8U7Xxakyuo='),
     new Recipe('This is a recipe', 'This is more detail about the recipe', 'https://media.istockphoto.com/id/1479912027/photo/stir-fry-chicken-with-paprika-mushrooms-green-chives-and-sesame-seeds-in-ceramic-bowl-asian.webp?s=2048x2048&w=is&k=20&c=cvK2WbLmumldC1gGJEPFAZ7fakIAJayVv8U7Xxakyuo=')
@@ -16,6 +17,9 @@ export class RecipeListsComponent implements OnInit {
 
   ngOnInit(): void {
     
+  }
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
